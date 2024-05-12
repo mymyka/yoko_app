fcd53be37b8088c5da569df33df530ed55b50f01
Mykyta Kasianenko <mykytakasianenko@gmail.com>
2024-05-12 13:35:49 +0300

    feat: button styles

--- lib/features/general/presentation/widgets/button.dart
+++ lib/features/general/presentation/widgets/button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yoko_app/utils/ext/ext.dart';
 
-class Button extends StatelessWidget {
+abstract class Button extends StatelessWidget {
   final String text;
   final Icon? icon;
   final void Function()? onTap;
@@ -10,7 +10,30 @@ class Button extends StatelessWidget {
     super.key,
     required this.text,
     this.icon,
-    required this.onTap,
+    this.onTap,
+  });
+
+  const factory Button.primary({
+    Key? key,
+    required String text,
+    Icon? icon,
+    void Function()? onTap,
+  }) = PrimaryButton;
+
+  const factory Button.outlined({
+    Key? key,
+    required String text,
+    Icon? icon,
+    void Function()? onTap,
+  }) = OutlinedButton;
+}
+
+class PrimaryButton extends Button {
+  const PrimaryButton({
+    super.key,
+    required super.text,
+    super.icon,
+    super.onTap,
   });
 
   @override
@@ -26,6 +49,7 @@ class Button extends StatelessWidget {
             width: 200.0,
             decoration: BoxDecoration(
               color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.circular(10.0),
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -33,7 +57,54 @@ class Button extends StatelessWidget {
                 if (icon != null) icon!,
                 Text(
                   text,
-                  style: Theme.of(context).textTheme.body,
+                  style: Theme.of(context).textTheme.body?.copyWith(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class OutlinedButton extends Button {
+  const OutlinedButton({
+    super.key,
+    required super.text,
+    super.icon,
+    super.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        elevation: 5.0,
+        child: InkWell(
+          onTap: onTap,
+          child: Container(
+            height: 50.0,
+            width: 200.0,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                if (icon != null) icon!,
+                Text(
+                  text,
+                  style: Theme.of(context).textTheme.body?.copyWith(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
                 ),
               ],
             ),
