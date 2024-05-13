914470ce41134ccaedddb8d8d9d4b7d7aff5552b
Mykyta Kasianenko <mykytakasianenko@gmail.com>
2024-05-13 14:54:03 +0300

    fix: dart fix

--- lib/features/auth/presentation/widgets/logo_app_bar.dart
+++ lib/features/auth/presentation/widgets/logo_app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:yoko_app/features/general/general.dart';
 import 'package:yoko_app/utils/ext/ext.dart';
 
