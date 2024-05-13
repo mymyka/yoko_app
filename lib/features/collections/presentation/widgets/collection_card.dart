cbe2f22a84ec59d23d4a8c9ba3c46da219489676
Mykyta Kasianenko <mykytakasianenko@gmail.com>
2024-05-13 18:00:54 +0300

    fix: collection card added onTap

--- lib/features/collections/presentation/widgets/collection_card.dart
+++ lib/features/collections/presentation/widgets/collection_card.dart
@@ -14,66 +14,69 @@ class CollectionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      color: Theme.of(context).colorScheme.background,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.network(
-                  collection.posterUrl,
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(width: 16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        elevation: 0,
+        color: Theme.of(context).colorScheme.background,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    collection.posterUrl,
                     width: 100,
-                    child: Text(
-                      collection.name,
-                      style: Theme.of(context).textTheme.bodyBold,
-                      overflow: TextOverflow.ellipsis,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 100,
+                      child: Text(
+                        collection.name,
+                        style: Theme.of(context).textTheme.bodyBold,
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
-                  ),
-                  Row(
-                    children: [
-                      const Icon(Icons.school_rounded),
-                      const SizedBox(width: 4),
-                      Text(
-                        "${collection.wordsLearned}",
-                        style: Theme.of(context).textTheme.bodyLight,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      const Icon(Icons.account_balance_rounded),
-                      const SizedBox(width: 4),
-                      Text(
-                        "${collection.wordsCount}",
-                        style: Theme.of(context).textTheme.bodyLight,
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              PercentIndicator(
-                  percent: collection.wordsLearned / collection.wordsCount),
-            ],
-          ),
-        ],
+                    Row(
+                      children: [
+                        const Icon(Icons.school_rounded),
+                        const SizedBox(width: 4),
+                        Text(
+                          "${collection.wordsLearned}",
+                          style: Theme.of(context).textTheme.bodyLight,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Icon(Icons.account_balance_rounded),
+                        const SizedBox(width: 4),
+                        Text(
+                          "${collection.wordsCount}",
+                          style: Theme.of(context).textTheme.bodyLight,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                PercentIndicator(
+                    percent: collection.wordsLearned / collection.wordsCount),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
